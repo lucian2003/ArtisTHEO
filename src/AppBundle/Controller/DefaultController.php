1502895699490badd12cc119765f645386fdc3a3
Lucian Salajan <54812721+lucian2003@users.noreply.github.com>
2020-09-02 12:21:38 +0200

    Modificación del Controlador
    Modificación del Controlador para que consulte los datos en la base de datos y los envie a la Vista.
    Modificación de la Vista para que muestre en la tabla la información que se recibe del controlador.

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,15 +2,26 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\artista;
+
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('AppBundle:default:index.html.twig');
+
+    	$artistas = $this->getDoctrine()
+            ->getRepository('AppBundle:artista')
+            ->findAll();
+
+        return $this->render('AppBundle:default:index.html.twig', array(
+            'artistas' => $artistas
+        ));
 
     }
 }
